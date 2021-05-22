f672f563a4e0d43075a0e41d1ea5ac6723319fe6
Digneli Davila <digneli14@gmail.com>
2021-05-22 17:05:48 -0300

    FMT package, not just print letters

--- src/main.go
+++ src/main.go
@@ -104,4 +104,27 @@ func main() {
 
 	fmt.Println(shortInt, longInt, shortFloat, longFloat, text,
 		boolean, complex)
+
+	helloMessage := "Hello"
+	worldMessage := "world"
+
+	// Println: Salto de Linea Automatico
+	fmt.Println(helloMessage, worldMessage)
+	fmt.Println(helloMessage, worldMessage)
+
+	// Printf
+	nombre := "La academia"
+	cursos := 500
+	// Con valores seguros
+	fmt.Printf("%s tiene más de %d cursos\n", nombre, cursos)
+	// Con valores inseguros
+	fmt.Printf("%v tiene más de %v cursos\n", nombre, cursos)
+
+	// Sprintf
+	message := fmt.Sprintf("%v tiene más de %v cursos\n", nombre, cursos)
+	fmt.Println(message)
+
+	// Tipo de datos:
+	fmt.Printf("helloMessage: %T\n", helloMessage)
+	fmt.Printf("cursos: %T\n", cursos)
 }
