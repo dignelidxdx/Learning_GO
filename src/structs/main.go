90d0f9ca9915f15c234e5dd634d5800bed546d46
Digneli Davila <digneli14@gmail.com>
2021-05-24 01:27:38 -0300

    added challenge and use functions for classes

--- src/structs/main.go
+++ src/structs/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+func (myPc pc) ping() {
+	fmt.Println(myPc.brand, "Pong")
+}
+
 type car struct {
 	brand   string
 	year    int
@@ -17,4 +27,18 @@ func main() {
 	var otherCar car
 	otherCar.brand = "Ferrari"
 	fmt.Println(otherCar)
+
+	a := 50
+	b := &a
+
+	fmt.Println(b)
+	fmt.Println(*b)
+
+	*b = 100
+	fmt.Println(a)
+
+	myPc := pc{ram: 16, disk: 200, brand: "msi"}
+	fmt.Println(myPc)
+
+	myPc.ping()
 }
